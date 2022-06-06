89774d92fc6fc96547d581bf2943a1d11a5c2bb2
Michael Lagomarsino <mike@radom.network>
2022-06-06 22:53:33 +0100

    Handler to add a tweet

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,4 +1,11 @@
-use actix_web::{delete, get, post, put, web, HttpResponse, Responder};
+use super::DbPool;
+
+use actix_web::{delete, get, post, put, web, Error, HttpResponse, Responder};
+use diesel::prelude::*;
+
+use crate::models::{NewTweet, Tweet, TweetPayload};
+
+type DbError = Box<dyn std::error::Error + Send + Sync>;
 
 #[get("/tweets")]
 async fn index() -> impl Responder {
@@ -6,8 +13,18 @@ async fn index() -> impl Responder {
 }
 
 #[post("/tweets")]
-async fn create() -> impl Responder {
-  HttpResponse::Ok().body("Tweet#new")
+async fn create(
+  pool: web::Data<DbPool>,
+  payload: web::Json<TweetPayload>,
+) -> Result<HttpResponse, Error> {
+  let tweet = web::block(move || {
+    let conn = pool.get()?;
+    add_a_tweet(&payload.message, &conn)
+  })
+  .await?
+  .map_err(actix_web::error::ErrorInternalServerError)?;
+
+  Ok(HttpResponse::Ok().json(tweet))
 }
 
 #[get("/tweets/{id}")]
@@ -23,4 +40,18 @@ async fn update(id: web::Path<String>) -> impl Responder {
 #[delete("/tweets/{id}")]
 async fn destroy(id: web::Path<String>) -> impl Responder {
   HttpResponse::Ok().body(format!("Tweet#delete {}", id))
+}
+
+fn add_a_tweet(_message: &str, conn: &PgConnection) -> Result<Tweet, DbError> {
+  use crate::schema::tweets::dsl::*;
+
+  let new_tweet = NewTweet {
+    message: _message,
+    created_at: chrono::Local::now().naive_local(),
+  };
+
+  let res = diesel::insert_into(tweets)
+    .values(&new_tweet)
+    .get_result(conn)?;
+  Ok(res)
 }
