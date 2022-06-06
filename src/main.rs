20376dd68723826e4f978b16e34bb7a20b145a4b
Michael Lagomarsino <mike@radom.network>
2022-06-06 21:51:56 +0100

    Use handlers

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,17 @@
-use actix_web::{delete, get, post, put, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer};
 
-#[get("/tweets")]
-async fn tweet_index() -> impl Responder {
-    HttpResponse::Ok().body("Tweet#index")
-}
-
-#[post("/tweets")]
-async fn tweet_create() -> impl Responder {
-    HttpResponse::Ok().body("Tweet#new")
-}
-
-#[get("/tweets/{id}")]
-async fn tweet_show(id: web::Path<String>) -> impl Responder {
-    HttpResponse::Ok().body(format!("Tweet#show {}", id))
-}
-
-#[put("/tweets/{id}")]
-async fn tweet_update(id: web::Path<String>) -> impl Responder {
-    HttpResponse::Ok().body(format!("Tweet#edit {}", id))
-}
-
-#[delete("/tweets/{id}")]
-async fn tweet_destroy(id: web::Path<String>) -> impl Responder {
-    HttpResponse::Ok().body(format!("Tweet#delete {}", id))
-}
+mod handlers;
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(|| async { "Actix REST API" }))
-            .service(tweet_index)
-            .service(tweet_create)
-            .service(tweet_show)
-            .service(tweet_update)
-            .service(tweet_destroy)
+            .service(handlers::index)
+            .service(handlers::create)
+            .service(handlers::show)
+            .service(handlers::update)
+            .service(handlers::destroy)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
