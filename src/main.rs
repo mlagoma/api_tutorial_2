7bb540a62cff12527fa5aa1d2e3405b1ec0d70d6
Michael Lagomarsino <mike@radom.network>
2022-06-06 22:29:16 +0100

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate diesel;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{middleware, web, App, HttpServer};
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
 
@@ -15,6 +15,8 @@ async fn main() -> std::io::Result<()> {
     // Loading .env into environment variable.
     dotenv::dotenv().ok();
 
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
     // set up database connection pool
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL");
     let manager = ConnectionManager::<PgConnection>::new(database_url);
@@ -25,6 +27,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
+            .wrap(middleware::Logger::default())
             .route("/", web::get().to(|| async { "Actix REST API" }))
             .service(handlers::index)
             .service(handlers::create)
